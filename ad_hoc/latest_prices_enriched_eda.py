2b1d826555f7710dd57a3c309df9d351ad068114
bendeininger1 <bendeininger1@gmail.com>
2026-02-02 21:12:49 +0000

    extend eda

--- ad_hoc/latest_prices_enriched_eda.py
+++ ad_hoc/latest_prices_enriched_eda.py
@@ -17,20 +17,23 @@ df.count()
 
 # COMMAND ----------
 
-df2 = df.filter("id IN ('1161')") #Adamant full helm
-df2 = df2.filter("time > 1769616000") # exclude older data
-df2 = df2.sort("NYCTime", ascending=False)
-df2.display()
+# MAGIC %skip
+# MAGIC df2 = df.filter("id IN ('1161')") #Adamant full helm
+# MAGIC df2 = df2.filter("time > 1769616000") # exclude older data
+# MAGIC df2 = df2.sort("NYCTime", ascending=False)
+# MAGIC # df2.display()
 
 # COMMAND ----------
 
-# std of buy prices
-df2.filter("highorlow = 'high'").select(std("price")).show()
+# MAGIC %skip
+# MAGIC # std of buy prices
+# MAGIC df2.filter("highorlow = 'high'").select(std("price")).show()
 
 # COMMAND ----------
 
-# std of sell prices
-df2.filter("highorlow = 'low'").select(std("price")).show()
+# MAGIC %skip
+# MAGIC # std of sell prices
+# MAGIC df2.filter("highorlow = 'low'").select(std("price")).show()
 
 # COMMAND ----------
 
@@ -56,25 +59,28 @@ df2.filter("highorlow = 'low'").select(std("price")).show()
 
 # COMMAND ----------
 
-nature_rune_cost = 100
-
-df3 = df.filter("time > 1769029986") # exclude older data
-df3 = df3.withColumn("alch_result", col("highalch") - lit(nature_rune_cost) - col("price"))
-df3 = df3.sort("alch_result", ascending=False)
-df3.display()
-
-# COMMAND ----------
-
-df4 = df.filter("id IN ('1161','1731','1725','1199','1073')")
-df4 = df4.filter("time > 1769701000") # exclude older data
-df4 = df4.sort("NYCTime", ascending=False)
-df4.display()
+# MAGIC %skip
+# MAGIC nature_rune_cost = 100
+# MAGIC
+# MAGIC df3 = df.filter("time > 1769029986") # exclude older data
+# MAGIC df3 = df3.withColumn("alch_result", col("highalch") - lit(nature_rune_cost) - col("price"))
+# MAGIC df3 = df3.sort("alch_result", ascending=False)
+# MAGIC # df3.display()
 
 # COMMAND ----------
 
-df5 = df4.groupBy("id","highorlow").agg(round(avg("price"), 2).alias("avg_price"), round(std("price"), 2).alias("std_price"))
-df5 = df5.sort("id")
-df5.display()
+# MAGIC %skip
+# MAGIC df4 = df.filter("id IN ('1161','1731','1725','1199','1073')")
+# MAGIC df4 = df4.filter("time > 1769701000") # exclude older data
+# MAGIC df4 = df4.sort("NYCTime", ascending=False)
+# MAGIC # df4.display()
+
+# COMMAND ----------
+
+# MAGIC %skip
+# MAGIC df5 = df4.groupBy("id","highorlow").agg(round(avg("price"), 2).alias("avg_price"), round(std("price"), 2).alias("std_price"))
+# MAGIC df5 = df5.sort("id")
+# MAGIC # df5.display()
 
 # COMMAND ----------
 
@@ -103,17 +109,40 @@ df7 = df6.groupBy("id","highorlow").agg(round(avg("price"), 2).alias("avg_price"
 #join with volume data
 df7 = df7.join(df_avg, "id").withColumn("std/avg", round(col("std_price")/col("avg_price"), 3))
 df7 = df7.join(df_map,"id")
+df7 = df7.withColumn("high_Alch_Margin", round((col("highalch") - col("avg_price") - 100),1))
 df7 = df7.withColumn("limit_X_std_price", col("std_price")*col("limit"))
 #df7.display()
 
 
 # COMMAND ----------
 
-# TODO add a column for highalch profits
-df8 = df7.filter("highorlow = 'high'").sort("std/avg", ascending=False)
-#df8.filter("std_price >= .5")
+df8 = df7.filter("highorlow = 'low'").sort("std/avg", ascending=False)
+df8 = df8.filter("std_price >= 5")
+df8 = df8.filter("avg1HourHighVolume > 1000")
 df8.display()
 
 # COMMAND ----------
 
-df8.display()
+# DBTITLE 1,Cell 18
+# Data from the last hour only
+df9 = df.filter((col("time") > unix_timestamp() - 3600) & (col("limit") >= 70))
+
+df10 = df9.groupBy("id","highorlow").agg(round(avg("price"), 2).alias("avg_price"), round(std("price"), 2).alias("std_price"))
+#join with volume data
+df10 = df10.join(df_avg, "id").withColumn("std/avg", round(col("std_price")/col("avg_price"), 3))
+df10 = df10.join(df_map,"id")
+df10 = df10.withColumn("high_Alch_Margin", round((col("highalch") - col("avg_price")) - 100,1))
+df10 = df10.withColumn("limit_X_std_price", col("std_price")*col("limit"))
+df10 = df10.withColumn("price-1hour_high_price", col("avg_price") - col("avg1HourHigh"))
+
+
+df10 = df10.filter("highorlow = 'low'")
+df10 = df10.filter("std_price >= 2.5")
+df10 = df10.filter("avg1HourHighVolume > 1000")
+df10 = df10.sort("std/avg", ascending=False)
+df10.display()
+
+# COMMAND ----------
+
+# MAGIC %skip
+# MAGIC df8.display()
