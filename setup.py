7776c5915802ae1397909c0354f5d8f1bd044087
Benjamin Deininger <57870564+bendeininger1@users.noreply.github.com>
2026-05-04 12:41:04 -0400

    Revert "Feature/migrate to uv package manager (#65)"
    This reverts commit 897886d9a18ff3c622db63f9a7a6103d3eaef385.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rs_project",
+    version="0.0.11",
+    description="This contains the code in the ./src directory of the project",
+    author="Ben Deininger",
+    packages=find_packages(where="./src"),
+    package_dir={"":"./src"},
+    install_requires=["setuptools"],
+    entry_points={
+        "packages":[
+            "main=rs_project.main:main"
+        ]
+    }
+)
