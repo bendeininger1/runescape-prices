fd02d0a79cf52ca2a4d605bcd88db691023157c4
Benjamin Deininger <57870564+bendeininger1@users.noreply.github.com>
2026-05-04 12:18:27 -0400

    Feature/migrate to uv package manager (#61)

--- tests/unit/test_fs_utils.py
+++ tests/unit/test_fs_utils.py
@@ -1,16 +1,19 @@
+import pytest
 import os
 import shutil
 from time import time
-from utils.fs_utils import archive_raw_files
+from src.utils.fs_utils import archive_raw_files
 from pathlib import Path
 
+@pytest.mark.unit_test
 def test_archive_raw_files():
     """
     """
 
     # set paths
-    source_path = "tests/data/fs_utils/archive_raw_files/source/"
-    dest_path = "tests/data/fs_utils/archive_raw_files/destination/"
+    root_path = "tests/unit/data/fs_utils/"
+    source_path = root_path + "archive_raw_files/source/"
+    dest_path = root_path + "archive_raw_files/destination/"
     time_filter = 5*60 #5 minutes
 
     # directory cleanup...
@@ -32,26 +35,26 @@ def test_archive_raw_files():
     # This file should not be moved based on the timestamp
     recent_file_name = "1m_prices_"+str(unix_timestamp)+".json"
 
-    # Copy both files to ../tests/data/fs_utils/archive/source for testing
-    shutil.copyfile("tests/data/fs_utils/archive_raw_files/original_file/1m_prices_1776697045.json",
-                    source_path+old_file_name)
-    shutil.copyfile("tests/data/fs_utils/archive_raw_files/original_file/1m_prices_1776697045.json",
-                    source_path+recent_file_name)
+    # Copy both files to ../tests/unit/data/fs_utils/archive/source for testing
+    shutil.copyfile(root_path + "archive_raw_files/original_file/1m_prices_1776697045.json",
+                    source_path + old_file_name)
+    shutil.copyfile(root_path + "archive_raw_files/original_file/1m_prices_1776697045.json",
+                    source_path + recent_file_name)
 
     archive_raw_files(source_path, dest_path, time_filter)
 
     # Original file should still be located here...
-    assertion1 = os.path.exists("tests/data/fs_utils/archive_raw_files/original_file/1m_prices_1776697045.json") #True
+    assertion1 = os.path.exists(root_path+"archive_raw_files/original_file/1m_prices_1776697045.json") #True
 
     # Old file should be in dest_path
-    assertion2 = os.path.exists("tests/data/fs_utils/archive_raw_files/destination/1m_prices_1776697045.json") #True
+    assertion2 = os.path.exists(dest_path + "1m_prices_1776697045.json") #True
     # Old file should not be in source_path
-    assertion3 = os.path.exists("tests/data/fs_utils/archive_raw_files/source/1m_prices_1776697045.json") #False
+    assertion3 = os.path.exists(source_path + "1m_prices_1776697045.json") #False
 
     # recent file should not be in dest_path
-    assertion4 = os.path.exists("tests/data/fs_utils/archive_raw_files/destination/" + recent_file_name) #False
+    assertion4 = os.path.exists(dest_path + recent_file_name) #False
     # recent file should be in source_path
-    assertion5 = os.path.exists("tests/data/fs_utils/archive_raw_files/source/" + recent_file_name) #True
+    assertion5 = os.path.exists(source_path + recent_file_name) #True
 
     assert (assertion1, assertion2, assertion3, assertion4, assertion5 )==(True, True, False, False, True)
 
