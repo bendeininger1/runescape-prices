4735eae31aa8148e4ea5bf43feba9352e7e4fb30
Ben Deininger <bendeininger1@gmail.com>
2026-05-04 15:38:04 -0400

    docs

--- tests/integration/test_basic_integration.py
+++ tests/integration/test_basic_integration.py
@@ -1,6 +1,7 @@
 import pytest
 import pyspark.sql.connect.session
 
+# Basic test to make sure the Spark session is running
 @pytest.mark.integration_test
 def test_get_spark(spark):
   assert isinstance(spark, pyspark.sql.connect.session.SparkSession)
